46fb50198a4df9f17a946fbbd22b49e9e92b7ac0
Marcus Rowe <undisbeliever@gmail.com>
2016-02-03 18:22:43 +1000

    feat: class for handling lists
    These classes will prevent a lot of code duplication that
    existed in the previous Qt version of the editor.

---
+++ src/models/common/namedlist.h
@@ -0,0 +1,240 @@
+#ifndef _UNTECH_MODELS_COMMON_NAMEDLIST_H_
+#define _UNTECH_MODELS_COMMON_NAMEDLIST_H_
+
+#include <map>
+#include <memory>
+#include <unordered_map>
+#include <utility>
+#include <string>
+
+namespace UnTech {
+
+inline static bool isNameListNameValid(const std::string& name)
+{
+    if (name.empty()) {
+        return false;
+    }
+
+    for (const char& c : name) {
+        if (!(c == '_' || (c >= '0' && c <= '9')
+              || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template <typename...>
+class NamedList;
+
+template <class T>
+class NamedList<T> {
+
+public:
+    NamedList()
+        : _values()
+        , _names()
+    {
+    }
+
+    std::shared_ptr<T> create(const std::string& name)
+    {
+        if (isNameListNameValid(name) && !nameExists(name)) {
+            auto e = std::make_shared<T>();
+
+            _values.insert({ name, e });
+            _names.insert({ e, name });
+
+            return e;
+        }
+
+        return nullptr;
+    }
+
+    std::shared_ptr<T> clone(std::shared_ptr<T> e, const std::string& newName)
+    {
+        if (isNameListNameValid(newName) && !nameExists(newName)) {
+            auto newElem = std::make_shared<T>(e);
+
+            _values.insert({ newName, newElem });
+            _names.insert({ newElem, newName });
+
+            return newElem;
+        }
+
+        return nullptr;
+    }
+
+    void remove(std::shared_ptr<T> e)
+    {
+        auto it = _names.find(e);
+
+        if (it != _names.end()) {
+            _values.erase(it->second);
+            _names.erase(it);
+        }
+    }
+
+    bool changeName(std::shared_ptr<T> e, const std::string& newName)
+    {
+        auto it = _names.find(e);
+
+        if (it != _names.end()) {
+            const std::string currentName = it->second;
+
+            if (newName != currentName) {
+                if (!isNameListNameValid(newName) || nameExists(newName)) {
+                    return false;
+                }
+
+                _values.erase(currentName);
+                _names.erase(it);
+
+                _values.insert({ newName, e });
+                _names.insert({ e, newName });
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool nameExists(std::string name) const
+    {
+        auto it = _values.find(name);
+        return it != _values.end();
+    }
+
+    std::pair<std::string, bool> getName(std::shared_ptr<T> e) const
+    {
+        auto it = _names.find(e);
+        if (it != _names.end()) {
+            return { it->first, true };
+        }
+
+        return { std::string(), false };
+    }
+
+    // Expose the map
+    inline auto size() const { return _values.size(); }
+    inline auto begin() const { return _values.begin(); }
+    inline auto end() const { return _values.end(); }
+    inline auto cbegin() const { return _values.cbegin(); }
+    inline auto cend() const { return _values.cend(); }
+
+private:
+    std::map<std::string, std::shared_ptr<T>> _values;
+    std::unordered_map<std::shared_ptr<T>, std::string> _names;
+};
+
+template <class P, class T>
+class NamedList<P, T> {
+
+public:
+    NamedList() = default;
+
+    NamedList(P& owner)
+        : _owner(owner)
+        , _values()
+        , _names()
+    {
+    }
+
+    std::shared_ptr<T> create(const std::string& name)
+    {
+        if (isNameListNameValid(name) && !nameExists(name)) {
+            std::shared_ptr<P> owner = _owner.ptr();
+            auto e = std::make_shared<T>(owner);
+
+            _values.insert({ name, e });
+            _names.insert({ e, name });
+
+            return e;
+        }
+
+        return nullptr;
+    }
+
+    std::shared_ptr<T> clone(std::shared_ptr<T> e, const std::string& newName)
+    {
+        if (isNameListNameValid(newName) && !nameExists(newName)) {
+            std::shared_ptr<P> owner = _owner.ptr();
+            auto newElem = std::make_shared<T>(owner, e);
+
+            _values.insert({ newName, newElem });
+            _names.insert({ newElem, newName });
+
+            return newElem;
+        }
+
+        return nullptr;
+    }
+
+    void remove(std::shared_ptr<T> e)
+    {
+        auto it = _names.find(e);
+
+        if (it != _names.end()) {
+            _values.erase(it->second);
+            _names.erase(it);
+        }
+    }
+
+    bool changeName(std::shared_ptr<T> e, const std::string& newName)
+    {
+        auto it = _names.find(e);
+
+        if (it != _names.end()) {
+            const std::string currentName = it->second;
+
+            if (newName != currentName) {
+                if (!isNameListNameValid(newName) || nameExists(newName)) {
+                    return false;
+                }
+
+                _values.erase(currentName);
+                _names.erase(it);
+
+                _values.insert({ newName, e });
+                _names.insert({ e, newName });
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool nameExists(std::string name) const
+    {
+        auto it = _values.find(name);
+        return it != _values.end();
+    }
+
+    std::pair<std::string, bool> getName(std::shared_ptr<T> e) const
+    {
+        auto it = _names.find(e);
+        if (it != _names.end()) {
+            return { it->first, true };
+        }
+
+        return { std::string(), false };
+    }
+
+    // Expose the map
+    inline auto size() const { return _values.size(); }
+    inline auto begin() const { return _values.begin(); }
+    inline auto end() const { return _values.end(); }
+    inline auto cbegin() const { return _values.cbegin(); }
+    inline auto cend() const { return _values.cend(); }
+
+private:
+    P& _owner;
+    std::map<std::string, std::shared_ptr<T>> _values;
+    std::unordered_map<std::shared_ptr<T>, std::string> _names;
+};
+}
+
+#endif
---
+++ src/models/common/orderedlist.h
@@ -0,0 +1,88 @@
+#ifndef _UNTECH_MODELS_COMMON_ORDEREDLIST_H_
+#define _UNTECH_MODELS_COMMON_ORDEREDLIST_H_
+
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace UnTech {
+
+template <class P, class T>
+class OrderedList {
+
+public:
+    OrderedList() = default;
+
+    OrderedList(P& owner)
+        : _owner(owner)
+        , _list()
+    {
+    }
+
+    std::shared_ptr<T> create()
+    {
+        std::shared_ptr<P> owner = _owner.ptr();
+        auto e = std::make_shared<T>(owner);
+        _list.push_back(e);
+
+        return e;
+    }
+
+    std::shared_ptr<T> clone(std::shared_ptr<T> e)
+    {
+        std::shared_ptr<P> owner = _owner.ptr();
+        auto newElem = std::make_shared<T>(*e, owner);
+        _list.push_back(newElem);
+
+        return newElem;
+    }
+
+    void remove(std::shared_ptr<T> e)
+    {
+        auto it = std::find(_list.begin(), _list.end(), e);
+
+        if (it != _list.end()) {
+            _list.erase(it);
+        }
+    }
+
+    void moveUp(std::shared_ptr<T> e)
+    {
+        auto it = std::find(_list.begin(), _list.end(), e);
+
+        if (it != _list.end()) {
+            if (it != _list.begin()) {
+                auto other = it - 1;
+
+                iter_swap(it, other);
+            }
+        }
+    }
+
+    void moveDown(std::shared_ptr<T> e)
+    {
+        auto it = std::find(_list.begin(), _list.end(), e);
+
+        if (it != _list.end()) {
+            auto other = it + 1;
+
+            if (other != _list.end()) {
+                iter_swap(it, other);
+            }
+        }
+    }
+
+    // Expose the list
+    inline auto size() const { return _list.size(); }
+    inline auto begin() const { return _list.begin(); }
+    inline auto end() const { return _list.end(); }
+    inline auto cbegin() const { return _list.cbegin(); }
+    inline auto cend() const { return _list.cend(); }
+
+private:
+    P& _owner;
+    std::vector<std::shared_ptr<T>> _list;
+};
+}
+
+#endif
