481398def43001d493e477b38e1ae91dea1f428a
Marcus Rowe <undisbeliever@gmail.com>
2016-08-03 19:08:41 +1000

    feat: GUI command line arguments

--- src/gui/untech-spriteimporter.cpp
+++ src/gui/untech-spriteimporter.cpp
@@ -1,17 +1,51 @@
 #include "view/sprite-importer/frame.h"
+#include <wx/cmdline.h>
 #include <wx/wx.h>
 
 class MyApp : public wxApp {
 public:
     virtual bool OnInit();
+    virtual void OnInitCmdLine(wxCmdLineParser& parser);
+    virtual bool OnCmdLineParsed(wxCmdLineParser& parser);
+};
+
+static const wxCmdLineEntryDesc cmdLineDesc[] = {
+    { wxCMD_LINE_SWITCH, "h", "help", "Show help text",
+      wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
+
+    { wxCMD_LINE_PARAM, nullptr, nullptr, "files",
+      wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL | wxCMD_LINE_PARAM_MULTIPLE },
+
+    { wxCMD_LINE_NONE, nullptr, nullptr, nullptr, wxCMD_LINE_VAL_NONE, 0 }
 };
 
 IMPLEMENT_APP(MyApp)
 
 bool MyApp::OnInit()
 {
-    auto* frame = new UnTech::View::SpriteImporter::Frame();
-    frame->Show(true);
+    SetAppDisplayName("UnTech SpriteImporter Editor");
+    SetAppName("untech-spriteimporter-gui");
+
+    return wxApp::OnInit();
+}
+
+void MyApp::OnInitCmdLine(wxCmdLineParser& parser)
+{
+    parser.SetDesc(cmdLineDesc);
+    parser.SetSwitchChars("-");
+}
+
+bool MyApp::OnCmdLineParsed(wxCmdLineParser& parser)
+{
+    for (unsigned i = 0; i < parser.GetParamCount(); i++) {
+        const std::string& filename = parser.GetParam(i).ToStdString();
+        UnTech::View::SpriteImporter::Frame::CreateOpen(filename);
+    }
+
+    if (parser.GetParamCount() == 0) {
+        auto* frame = new UnTech::View::SpriteImporter::Frame();
+        frame->Show(true);
+    }
 
     return true;
 }
