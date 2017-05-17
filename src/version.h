16ce17f4e7a2167926572ece3b86164795f3bcca
Marcus Rowe <undisbeliever@gmail.com>
2017-05-17 17:17:56 +1000

    chore: update copyright year and holder
    Have decided to include my real name as it is already out there.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,4 @@
 #pragma once
-#include <array>
-#include <string>
 
 #define UNTECH_NAME "UnTech"
 #define UNTECH_VERSION "0.24"
@@ -9,4 +7,4 @@ const unsigned UNTECH_VERSION_INT = 24;
 #define UNTECH_LICENSE "The MIT License"
 #define UNTECH_LICENSE_URL "https://opensource.org/licenses/MIT"
 #define UNTECH_URL "http://undisbeliever.net/untech"
-#define UNTECH_COPYRIGHT "Copyright (c) 2016 The UnDisbeliever"
+#define UNTECH_COPYRIGHT "Copyright (c) 2016 - 2017, Marcus Rowe <undisbeliever@gmail.com>"
