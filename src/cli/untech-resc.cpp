9712b83fd1814071f206fede71b4fd09b47d1f71
Marcus Rowe <undisbeliever@gmail.com>
2018-02-08 13:11:46 +1000

    Update copyright year

--- src/cli/untech-resc.cpp
+++ src/cli/untech-resc.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of the UnTech Editor Suite.
- * Copyright (c) 2016 - 2017, Marcus Rowe <undisbeliever@gmail.com>.
+ * Copyright (c) 2016 - 2018, Marcus Rowe <undisbeliever@gmail.com>.
  * Distributed under The MIT License: https://opensource.org/licenses/MIT
  */
 
