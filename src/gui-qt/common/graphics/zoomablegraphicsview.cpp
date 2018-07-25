826b4ba40d5f0ff2d77045ee5658f62e8326f030
Marcus Rowe <undisbeliever@gmail.com>
2018-07-25 19:47:18 +1000

    Add Ctrl+ScrollWheel zooming to ZoomableGraphicsView

--- src/gui-qt/common/graphics/zoomablegraphicsview.cpp
+++ src/gui-qt/common/graphics/zoomablegraphicsview.cpp
@@ -7,18 +7,28 @@
 #include "zoomablegraphicsview.h"
 #include "zoomsettings.h"
 
+#include <QScrollBar>
+#include <QWheelEvent>
+
 using namespace UnTech::GuiQt;
 
 ZoomableGraphicsView::ZoomableGraphicsView(QWidget* parent)
     : QGraphicsView(parent)
     , _zoomSettings(nullptr)
+    , _enableZoomWithMouseWheel(true)
 {
 }
 
 ZoomableGraphicsView::ZoomableGraphicsView(QGraphicsScene* scene, QWidget* parent)
     : QGraphicsView(scene, parent)
     , _zoomSettings(nullptr)
+    , _enableZoomWithMouseWheel(true)
 {
+}
+
+void ZoomableGraphicsView::setEnableZoomWithMouseWheel(bool enableZoomWithMouseWheel)
+{
+    _enableZoomWithMouseWheel = enableZoomWithMouseWheel;
 }
 
 void ZoomableGraphicsView::setZoomSettings(ZoomSettings* zoomSettings)
@@ -39,6 +49,34 @@ void ZoomableGraphicsView::setZoomSettings(ZoomSettings* zoomSettings)
     }
     else {
         resetTransform();
+    }
+}
+
+void ZoomableGraphicsView::wheelEvent(QWheelEvent* event)
+{
+    if (_zoomSettings
+        && _enableZoomWithMouseWheel
+        && event->modifiers() & Qt::ControlModifier) {
+
+        const qreal oldZoom = _zoomSettings->zoom();
+        const QPointF oldScenePos = mapToScene(event->pos());
+
+        if (event->angleDelta().y() > 0) {
+            _zoomSettings->zoomIn();
+        }
+        else {
+            _zoomSettings->zoomOut();
+        }
+
+        if (_zoomSettings->zoom() != oldZoom) {
+            const QPoint delta = event->pos() - mapFromScene(oldScenePos);
+
+            horizontalScrollBar()->setValue(horizontalScrollBar()->value() - delta.x());
+            verticalScrollBar()->setValue(verticalScrollBar()->value() - delta.y());
+        }
+    }
+    else {
+        QGraphicsView::wheelEvent(event);
     }
 }
 
