80065ad6ebc162562e198683c6e1da076c066e3c
Marcus Rowe <undisbeliever@gmail.com>
2018-09-18 18:42:37 +1000

    Add Compare template argument to vectorset

---
+++ src/models/common/vectorset-upoint.h
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the UnTech Editor Suite.
+ * Copyright (c) 2016 - 2018, Marcus Rowe <undisbeliever@gmail.com>.
+ * Distributed under The MIT License: https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+
+#include "aabb.h"
+#include "vectorset.h"
+
+namespace UnTech {
+
+struct upoint_compare {
+    bool operator()(const upoint& a, const upoint& b)
+    {
+        return std::tie(a.x, a.y) < std::tie(b.x, b.y);
+    }
+};
+
+using upoint_vectorset = vectorset<upoint, upoint_compare>;
+}
