b6ab6fac7a48f38fef171e0cbdc5d2b2280c08b2
Marcus Rowe <undisbeliever@gmail.com>
2019-02-18 17:50:08 +1000

    Improve optional<T> class
     * Add support for optional references
     * Add specialized bad_optional_access exception
     * Add assignment operator
     * Add () -> and * operators
     * Add non-const getter

---
+++ src/models/common/optional.cpp
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the UnTech Editor Suite.
+ * Copyright (c) 2016 - 201, Marcus Rowe <undisbeliever@gmail.com>.
+ * Distributed under The MIT License: https://opensource.org/licenses/MIT
+ */
+
+#include "optional.h"
+#include "models/resources/palette.h"
+
+using namespace UnTech;
+
+// Test template compliles properly
+template class UnTech::optional<long>;
+template class UnTech::optional<Resources::PaletteInput&>;
+
+const char* bad_optional_access::what() const noexcept
+{
+    return "Bad optional access";
+}
--- src/models/common/optional.h
+++ src/models/common/optional.h
@@ -10,29 +10,53 @@
 
 namespace UnTech {
 
+class bad_optional_access : public std::exception {
+public:
+    explicit bad_optional_access() = default;
+    virtual const char* what() const noexcept final;
+};
+
+// Should only be used with simple types
 template <typename T>
 class optional {
 public:
-    ~optional() = default;
-    optional(const optional&) = default;
-    optional(optional&&) = default;
-    optional& operator=(const optional&) = default;
-    optional& operator=(optional&&) = default;
-
     optional()
         : _value()
         , _exists(false)
     {
     }
 
-    optional(const T& v)
-        : _value(v)
+    optional(T v)
+        : _value(std::move(v))
         , _exists(true)
     {
     }
 
+    ~optional() = default;
+    optional(const optional&) = default;
+    optional(optional&&) = default;
+    optional& operator=(const optional&) = default;
+    optional& operator=(optional&&) = default;
+
+    inline constexpr optional& operator=(T v)
+    {
+        _value = std::move(v);
+        _exists = true;
+        return *this;
+    }
+
     bool exists() const { return _exists; }
     explicit operator bool() const { return _exists; }
+
+    inline T& value()
+    {
+        if (_exists) {
+            return _value;
+        }
+        else {
+            throw bad_optional_access();
+        }
+    }
 
     inline const T& value() const
     {
@@ -40,36 +64,88 @@ public:
             return _value;
         }
         else {
-            throw std::runtime_error("Bad optional access");
+            throw bad_optional_access();
         }
     }
 
     inline const T& value_or(const T& defaultValue) const
     {
-        if (_exists) {
-            return _value;
-        }
-        else {
-            throw defaultValue;
-        }
+        return _exists ? _value : defaultValue;
     }
 
-    inline constexpr optional& operator=(const T& v)
-    {
-        _value = v;
-        _exists = true;
-        return *this;
-    }
+    inline T* operator->() { return &value(); }
+    inline const T* operator->() const { return &value(); }
+    inline T& operator*() { return value(); }
+    inline const T& operator*() const { return value(); }
 
-    inline constexpr optional& operator=(T&& v)
-    {
-        _value = v;
-        _exists = true;
-        return *this;
-    }
+    inline T& operator()() { return value(); }
+    inline const T& operator()() const { return value(); }
+    inline const T& operator()(const T& defaultValue) const { return value_or(defaultValue); }
 
 private:
     T _value;
     bool _exists;
+};
+
+// Once created an optional reference cannot be changed, only moved.
+template <typename T>
+class optional<T&> {
+public:
+    optional()
+        : _ptr(nullptr)
+    {
+    }
+
+    optional(T& v)
+        : _ptr(&v)
+    {
+    }
+
+    ~optional() = default;
+    optional(optional&&) = default;
+
+    optional(const optional&) = delete;
+    optional& operator=(const optional&) = delete;
+    optional& operator=(optional&&) = delete;
+
+    bool exists() const { return _ptr != nullptr; }
+    explicit operator bool() const { return _ptr != nullptr; }
+
+    inline const T& value() const
+    {
+        if (_ptr) {
+            return *_ptr;
+        }
+        else {
+            throw bad_optional_access();
+        }
+    }
+
+    inline T& value()
+    {
+        if (_ptr) {
+            return *_ptr;
+        }
+        else {
+            throw bad_optional_access();
+        }
+    }
+
+    inline const T& value_or(const T& defaultValue) const
+    {
+        return _ptr ? *_ptr : defaultValue;
+    }
+
+    inline T* operator->() { return &value(); }
+    inline const T* operator->() const { return &value(); }
+    inline T& operator*() { return value(); }
+    inline const T& operator*() const { return value(); }
+
+    inline T& operator()() { return value(); }
+    inline const T& operator()() const { return value(); }
+    inline const T& operator()(const T& defaultValue) const { return _ptr ? *_ptr : defaultValue; }
+
+private:
+    T* const _ptr;
 };
 }
