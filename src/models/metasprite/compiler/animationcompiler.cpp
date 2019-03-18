undisbeliever/untech-editor

commit aaf65921ee287ec1e7e89b42e0b689e1c5979cd3

Refactor to store MS animations and frames in a NamedList

src/models/metasprite/compiler/animationcompiler.cpp
@@ -45,14 +45,11 @@ static std::vector<uint8_t> processAnimation(const AnimationListEntry& aniEntry,
     uint8_t nextAnimationId = 0xff;
     {
         if (animation.oneShot == false) {
-            const ANI::Animation* a = &animation;
-
             if (animation.nextAnimation.isValid()) {
-                a = frameSet.animations.getPtr(animation.nextAnimation);
-                assert(a != nullptr);
+                assert(frameSet.animations.find(animation.nextAnimation));
             }
 
-            nextAnimationId = indexOf_throw(animations, { a, aniEntry.hFlip, aniEntry.vFlip });
+            nextAnimationId = indexOf_throw(animations, { &animation, aniEntry.hFlip, aniEntry.vFlip });
         }
     }
 
@@ -66,7 +63,7 @@ static std::vector<uint8_t> processAnimation(const AnimationListEntry& aniEntry,
     for (const auto& aFrame : animation.frames) {
         const auto& frameRef = aFrame.frame;
 
-        uint8_t frameId = indexOf_throw(frames, { frameSet.frames.getPtr(frameRef.name),
+        uint8_t frameId = indexOf_throw(frames, { &*frameSet.frames.find(frameRef.name),
                                                   static_cast<bool>(frameRef.hFlip ^ aniEntry.hFlip),
                                                   static_cast<bool>(frameRef.vFlip ^ aniEntry.vFlip) });
 

commit 1d453ecd3f8b20dcb948b49de9b22b5cda234cbb

Fix bug with nextAnimation in MS Animation Compiler
This commit fixes a bug in the MetaSprite Animation Compiler that
ignored the value of the nextAnimation field.

The bug was introduced in commit aaf6592[1] and wasn't caught as I only
tested the commit with horizontal-shooter and not the untech-engine unit
tests.

[^1]: Commit aaf65921ee287ec1e7e89b42e0b689e1c5979cd3
      Refactor to store MS animations and frames in a NamedList

src/models/metasprite/compiler/animationcompiler.cpp
@@ -45,11 +45,12 @@ static std::vector<uint8_t> processAnimation(const AnimationListEntry& aniEntry,
     uint8_t nextAnimationId = 0xff;
     {
         if (animation.oneShot == false) {
+            const ANI::Animation* nextAnimation = &animation;
             if (animation.nextAnimation.isValid()) {
-                assert(frameSet.animations.find(animation.nextAnimation));
+                nextAnimation = &frameSet.animations.find(animation.nextAnimation).value();
             }
 
-            nextAnimationId = indexOf_throw(animations, { &animation, aniEntry.hFlip, aniEntry.vFlip });
+            nextAnimationId = indexOf_throw(animations, { nextAnimation, aniEntry.hFlip, aniEntry.vFlip });
         }
     }
 